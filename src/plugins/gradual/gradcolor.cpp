89dddee064f0961cdd9abcf7cc37934811a71fed
tyw66 <2639144635@qq.com>
2019-05-06 17:41:30 +0800

    color grad

--- src/plugins/gradual/gradcolor.cpp
+++ src/plugins/gradual/gradcolor.cpp
@@ -8,20 +8,31 @@ GradColor::GradColor(QObject *parent)
 
 void GradColor::paintImg(QImage &img, const Param &param)
 {
-    int w = img.width();
-    int h = img.height();
 
 //#pragma omp parallel for
     //遍历像素点
-    for(int i = 0; i < h; ++i){
-        for(int j = 0; j < w; ++j){
+    for(int i = 0; i < img.width(); ++i){
+        for(int j = 0; j < img.width(); ++j){
             //获取颜色
-            int r = 155*i/h+100*param.mouse_posY/h;
-            r = r > 255 ? 255 : r;
-            int g = 155*j/w+100*param.mouse_posX/w;
-            g = g > 255 ? 255 : g;
+//            int r = 155*i/img.height() +100*param.mouse_posY/h;
+//            r = r > 255 ? 255 : r;
+//            int g = 155*j/ img.width()+100*param.mouse_posX/w;
+//            g = g > 255 ? 255 : g;
             //设置颜色
-            img.setPixel(j,i,QColor::fromRgb(r, g,140).rgb());
+           // img.setPixel(j,i,QColor::fromRgb(r,g,b).rgb());
+
+            ////////////////////////////////////////////////////////
+
+            double fw = j * 1.0 / img.width();
+            double fh = i * 1.0 / img.height();
+
+            int valueX = fw * 255;
+            int valueY = fh * 255;
+
+            int valueZ = param.mouse_posY *1.0 / param.drawArea_height * 255;
+
+            //设置颜色
+            img.setPixel(j,i,QColor::fromRgb(valueX, valueZ, valueY).rgb());
         }
     }
 }
